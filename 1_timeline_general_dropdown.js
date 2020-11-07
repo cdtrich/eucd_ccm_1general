b87c58e148ab30b82c936ed4bf5b0db8792c1799
cd_trich <31612691+cdtrich@users.noreply.github.com>
2020-11-07 12:20:40 +0100

    update update function, no dropdown integration yet

--- 1_timeline_general_dropdown.js
+++ 1_timeline_general_dropdown.js
@@ -13,6 +13,7 @@
 ///////////////////////////////////////////////////////////////////////////
 
 // dynamic dropdown selection: http://bl.ocks.org/williaster/10ef968ccfdc71c30ef8
+// or here http://bl.ocks.org/jhubley/17aa30fd98eb0cc7072f
 // modules for wp embed
 
 ///////////////////////////////////////////////////////////////////////////
@@ -25,25 +26,23 @@ console.clear();
 // https://github.com/janl/mustache.js
 // const Mustache = require("mustache");
 
-// if element already exists, return selection
-// if it doesn't exist, create it and give it class
-const selectOrCreate = (elementType, className, parent) => {
-	const selection = parent.select("." + className);
-	if (!selection.empty()) return selection;
-	return parent.append(elementType).attr("class", className);
-};
+///////////////////////////////////////////////////////////////////////////
+//////////////////////////// dropdown /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+// generate initial graph
+// data = filterJSON(json, 'produce', 'apples');
+// updateGraph(data);
 
 ///////////////////////////////////////////////////////////////////////////
 //////////////////////////// drawing function /////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 
 const createChart = async () => {
-	///////////////////////////////////////////////////////////////////////////
-	//////////////////////////// data /////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
-
 	// const url = "https://eucyberdirect.eu/wp-content/uploads/2020/11/cpi_cyber_operations_database_2020_version-1.0.csv";
 	const url = "./data/CPI_Cyber_Operations_Database_2020_Version 1.0.csv";
+
+	//////////////////////////// data /////////////////////////////////////////
 
 	let data = await d3.csv(url, (d) => {
 		return {
@@ -91,262 +90,286 @@ const createChart = async () => {
 	});
 	// console.log(data);
 
-	///////////////////////////////////////////////////////////////////////////
+	//////////////////////////// accessors ////////////////////////////////////
+
+	const xAccessor = (d) => d.startYear;
+	// const yAccessor = (d) => d.humidity;
+	const cAccessor = (d) => d.attacker_jurisdiction;
+	// const cAccessor = (d) => d.windSpeed;
+	// const rAccessor = (d) => d.temperatureMax;
+	// const oAccessor = (d) => d.temperatureMax;
+
 	//////////////////////////// Set up svg ///////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
 
-	const width = d3.min([window.innerWidth * 0.9, window.innerHeight * 0.9]),
-		height = d3.min([window.innerWidth * 0.33, window.innerHeight * 0.33]),
-		radius = width / 70,
-		margin = { top: 20, right: 50, bottom: 20, left: 50 };
-	// const svg = d3.create("svg")
-	// .attr("viewBox", [0, 0, width, height]);
+	const wrapper = d3.select("#wrapper").append("svg");
 
-	const svg = d3
-		.select("#app") // id app
-		.append("svg")
-		.attr("viewBox", [margin.left / 2, margin.right, width, height])
-		.style("overflow", "visible");
+	// if element already exists, return selection
+	// if it doesn't exist, create it and give it class
+	const selectOrCreate = (elementType, className, parent) => {
+		const selection = parent.select("." + className);
+		if (!selection.empty()) return selection;
+		return parent.append(elementType).attr("class", className);
+	};
 
-	// template
-	var template = d3.select("#template").html();
-	Mustache.parse(template);
-
-	///////////////////////////////////////////////////////////////////////////
-	//////////////////////////// data /////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
-
-	// pretty dates
-	var formatTime = d3.timeFormat("%d %b %Y");
-	data.forEach((d) => {
-		d.reportLabel = formatTime(d.report);
-	});
-
-	// fixing incomplete data
-	data.forEach((d) => {
-		switch (d.name) {
-			case "Stuxnet":
-				d.startYear = 2010;
-				break;
-			case "DDoS on US Banks":
-				d.reportLabel = "2012";
-				break;
-			default:
-		}
-	});
+	//////////////////////////// dropdown ///////////////////////////////////
+	// d3.select("#dropdown").on("change", function () {
+	// 	// element
+	// 	var sect = document.getElementById("dropdown");
+	// 	// value
+	// 	var section = sect.options[sect.selectedIndex].value;
+	//
+	// 	createChart(data);
+	// });
 
 	///////////////////////////////////////////////////////////////////////////
-	//////////////////////////// scales ///////////////////////////////////////
+	//////////////////////////// update ///////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////
 
-	const colorsType = [
-		"#113655",
-		"#f28c00",
-		"#3f8ca5",
-		"#fab85f",
-		"#99d4e3",
-		"#fed061"
-	];
+	const update = () => {
+		//////////////////////////// sizes ///////////////////////////////////
+		const size = d3.min([window.innerWidth * 0.9, window.innerHeight * 0.9]);
 
-	const wrap = (s, w) =>
-		s
-			.toString()
-			.replace(
-				new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, "g"),
-				"$1\n"
-			);
+		let dimensions = {
+			width: size,
+			height: size * 0.66,
+			margin: {
+				top: 15,
+				right: 15,
+				bottom: 60,
+				left: 60
+			}
+		};
 
-	var dataType = _.chain(data)
-		.map((d) => d.attacker_jurisdiction)
-		.uniq()
-		.value();
+		const radius = dimensions.width / 70;
 
-	// weirdly doesn't show in legend
-	var dataTypeLabel = dataType;
-	// var dataTypeLabel = dataType.map((d) => wrap(d, 20));
-	// var dataTypeLabel = dataType.map((d) => d + "wrap");
-	// console.log(dataTypeLabel);
+		dimensions.boundedWidth =
+			dimensions.width - dimensions.margin.left - dimensions.margin.right;
+		dimensions.boundedHeight =
+			dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
-	const xScale = d3
-		.scaleLinear()
-		.domain(
-			d3.extent(data, (d) => {
-				return d.startYear;
-			})
-		)
-		.range([margin.left, width - margin.right]);
+		//////////////////////////// svg ///////////////////////////////////
 
-	const cScale = d3.scaleOrdinal().domain(dataTypeLabel).range(colorsType);
+		// tag = name; class = .name; id = #name;
+		wrapper.attr("width", dimensions.width).attr("height", dimensions.height);
 
-	var simulation = d3
-		.forceSimulation(data)
-		.force(
-			"x",
-			d3
-				.forceX(function (d) {
-					return xScale(d.startYear);
-				})
-				.strength(0.95)
-		)
-		.force("y", d3.forceY(height).strength(0.05))
-		.force("collide", d3.forceCollide(radius * 1.5))
-		.stop();
+		// shifting
+		const bounds = selectOrCreate("g", "wrapper", wrapper).style(
+			"transform",
+			`translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`
+		);
 
-	for (var i = 0; i < 10; ++i) simulation.tick();
+		// template
+		var template = d3.select("#template").html();
+		Mustache.parse(template);
 
-	///////////////////////////////////////////////////////////////////////////
-	//////////////////////////// plot /////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
+		//////////////////////////// data /////////////////////////////////////////
 
-	// dots
-	const dots = svg
-		.selectAll(".dots")
-		.data(data)
-		.enter()
-		// cell
-		.append("circle")
-		.attr("class", "dots")
-		.attr("r", 0)
-		.attr("cx", (d) => d.x)
-		.attr("cy", 0)
-		.style("opacity", 0);
+		// pretty dates
+		var formatTime = d3.timeFormat("%d %b %Y");
+		data.forEach((d) => {
+			d.reportLabel = formatTime(d.report);
+		});
 
-	dots
-		.transition()
-		.duration((d, i) => i * 50)
-		.attr("r", radius)
-		.attr("cx", (d) => d.x)
-		.attr("cy", (d) => d.y)
-		.attr("fill", (d) => cScale(d.attacker_jurisdiction))
-		.style("opacity", 1);
-
-	// tooltip
-	// const tooltip = svg.append("div").classed("tooltip", true);
-	// const tooltip = selectOrCreate("div", "tooltip", d3.select("#app"));
-
-	dots.on("mouseover", (event, d) => {
-		var mouseX = event.pageX + 5;
-		var mouseY = event.pageY + 5;
-		// var x = d.x;
-		// var y = d.y;
-		// tooltip
-		d3.select(".tooltip")
-			.style("visibility", "visible")
-			.style("opacity", 1)
-			// 	.style(
-			// 		"transform",
-			// 		`translate(
-			// calc(${x + margin.left}px - 50%),
-			// calc(${y + margin.top}px - 5%))`
-			// );
-			.style("left", mouseX + "px")
-			.style("top", mouseY + "px")
-			.text(d.name);
-		// smoother change in opacity
-		dots.transition().style("opacity", 0.5);
-		// d3.select(event.target).style("opacity", 1);
-		// d3.select(".tooltip h2").text(d.name);
-	});
-
-	dots.on("mousemove", (d, i) => {
-		var mouseX = event.pageX + 5;
-		var mouseY = event.pageY + 5;
-		// var x = d.x;
-		// var y = d.y;
-		d3.select(".tooltip")
-			// 	// tooltip
-			// 	.style(
-			// 		"transform",
-			// 		`translate(
-			// calc(${x + margin.left}px - 50%),
-			// calc(${y + margin.top}px - 5%))`
-			// 	)
-			.style("left", mouseX + "px")
-			.style("top", mouseY + "px")
-			.text(d.name);
-	});
-
-	dots.on("mouseleave", function (d) {
-		d3.select(".tooltip").style("visibility", "hidden");
-		dots.transition().style("opacity", 1);
-	});
-
-	dots.on("click", on);
-
-	///////////////////////////////////////////////////////////////////////////
-	//////////////////////////// axes /////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
-
-	// axes
-	var formatAxis = d3.format(".4r");
-
-	const xAxis = d3.axisBottom().scale(xScale).tickFormat(formatAxis);
-
-	svg
-		.append("g")
-		.classed("x-axis", true)
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis);
-
-	///////////////////////////////////////////////////////////////////////////
-	//////////////////////////// details //////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
-
-	var dataL = 0;
-	var legendOffset = (width - 100) / dataType.length;
-
-	var legend = d3
-		.select(".legend")
-		// or svg.append("g")
-		.append("svg")
-		.attr("width", width)
-		.attr("height", radius * 2);
-
-	var drawLegend = legend
-		.selectAll(".legend")
-		.data(dataTypeLabel)
-		.enter()
-		.append("g")
-		.attr("class", "legend")
-		.attr("transform", function (d, i) {
-			if (i === 0) {
-				dataL = d.length + legendOffset;
-				return "translate(0,0)";
-			} else {
-				var newdataL = dataL;
-				dataL += d.length + legendOffset;
-				return "translate(" + newdataL + ",0)";
+		// fixing incomplete data
+		data.forEach((d) => {
+			switch (d.name) {
+				case "Stuxnet":
+					d.startYear = 2010;
+					break;
+				case "DDoS on US Banks":
+					d.reportLabel = "2012";
+					break;
+				default:
 			}
 		});
 
-	drawLegend
-		.append("circle")
-		.attr("cx", radius)
-		.attr("cy", radius)
-		.attr("r", radius / 2)
-		.style("fill", (d, i) => colorsType[i]);
+		//////////////////////////// scales ///////////////////////////////////////
 
-	drawLegend
-		.append("text")
-		.attr("x", radius + radius)
-		.attr("y", radius * 1.5)
-		.text((d) => d)
-		.attr("class", "textselected")
-		.style("text-anchor", "start");
+		const wrap = (s, w) =>
+			s
+				.toString()
+				.replace(
+					new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, "g"),
+					"$1\n"
+				);
 
-	///////////////////////////////////////////////////////////////////////////
-	//////////////////////////// details //////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
+		const colorsType = [
+			"#113655",
+			"#f28c00",
+			"#3f8ca5",
+			"#fab85f",
+			"#99d4e3",
+			"#fed061"
+		];
 
-	function on(f) {
-		document.getElementById("overlay").style.display = "block";
-		var detailsHtml = Mustache.render(template, f);
-		d3.select("#overlay").html(detailsHtml);
-	}
+		var dataType = _.chain(data)
+			.map((d) => d.attacker_jurisdiction)
+			.uniq()
+			.value();
 
-	// function off() {
-	// 	document.getElementById("overlay").style.display = "none";
-	// }
+		// weirdly doesn't show in legend
+		var dataTypeLabel = dataType;
+		// var dataTypeLabel = dataType.map((d) => wrap(d, 20));
+		// var dataTypeLabel = dataType.map((d) => d + "wrap");
+		// console.log(dataTypeLabel);
+
+		const xScale = d3
+			.scaleLinear()
+			.domain(d3.extent(data, xAccessor))
+			.range([0, dimensions.boundedWidth]);
+
+		const cScale = d3.scaleOrdinal().domain(dataTypeLabel).range(colorsType);
+
+		var simulation = d3
+			.forceSimulation(data)
+			.force(
+				"x",
+				d3
+					.forceX(function (d) {
+						return xScale(d.startYear);
+					})
+					.strength(0.95)
+			)
+			.force("y", d3.forceY(dimensions.boundedHeight).strength(0.05))
+			.force("collide", d3.forceCollide(radius * 1.5))
+			.stop();
+
+		for (var i = 0; i < 10; ++i) simulation.tick();
+
+		//////////////////////////// axes /////////////////////////////////////////
+
+		var formatAxis = d3.format(".4r");
+
+		const xAxisGenerator = d3.axisBottom().scale(xScale).tickFormat(formatAxis);
+		const xAxis = selectOrCreate("g", "xAxis", bounds)
+			.transition()
+			.call(xAxisGenerator)
+			.style("transform", `translate(0,${dimensions.boundedHeight}px)`);
+
+		//////////////////////////// plot /////////////////////////////////////////
+
+		// starting position
+		const dots = (data) => {
+			const tooltip = selectOrCreate("div", "tooltip", d3.select("#wrapper"));
+
+			const dots = bounds
+				.selectAll(".dots")
+				.data(data)
+				.enter()
+				// cell
+				.append("circle")
+				.attr("class", "dots")
+				.attr("r", 0)
+				.attr("cx", (d) => d.x)
+				.attr("cy", 0)
+				.style("opacity", 0);
+
+			// animated drop
+			dots
+				.transition()
+				.duration((d, i) => i * 50)
+				.attr("r", radius)
+				.attr("cx", (d) => d.x)
+				.attr("cy", (d) => d.y)
+				.attr("fill", (d) => cScale(d.attacker_jurisdiction))
+				.style("opacity", 1);
+
+			// tooltip
+			dots.on("mouseover", (event, d) => {
+				var mouseX = event.pageX + 5;
+				var mouseY = event.pageY + 5;
+				d3.select(".tooltip")
+					.style("visibility", "visible")
+					.style("opacity", 1)
+					.style("left", mouseX + "px")
+					.style("top", mouseY + "px")
+					.text(d.name);
+				// smoother change in opacity
+				dots.transition().style("opacity", 0.5);
+			});
+
+			dots.on("mousemove", (d, i) => {
+				var mouseX = event.pageX + 5;
+				var mouseY = event.pageY + 5;
+				d3.select(".tooltip")
+					.style("left", mouseX + "px")
+					.style("top", mouseY + "px")
+					.text(d.name);
+			});
+
+			dots.on("mouseleave", function (d) {
+				d3.select(".tooltip").style("visibility", "hidden");
+				dots.transition().style("opacity", 1);
+			});
+
+			// overlay
+			dots.on("click", on);
+
+			///////////////////////////////////////////////////////////////////////////
+			//////////////////////////// details //////////////////////////////////////
+			///////////////////////////////////////////////////////////////////////////
+
+			var dataL = 0;
+			var legendOffset = (dimensions.boundedWidth - 100) / dataType.length;
+
+			var legend = selectOrCreate("g", "legend", bounds)
+				.attr("width", dimensions.boundedWidth)
+				.attr("height", radius * 2);
+
+			var drawLegend = legend
+				.selectAll(".legend")
+				.data(dataTypeLabel)
+				.enter()
+				.append("g")
+				.attr("class", "legend")
+				.attr("transform", function (d, i) {
+					if (i === 0) {
+						dataL = d.length + legendOffset;
+						return "translate(0,0)";
+					} else {
+						var newdataL = dataL;
+						dataL += d.length + legendOffset;
+						return "translate(" + newdataL + ",0)";
+					}
+				});
+
+			drawLegend
+				.append("circle")
+				.attr("cx", radius)
+				.attr("cy", radius)
+				.attr("r", radius / 2)
+				.style("fill", (d, i) => colorsType[i]);
+
+			drawLegend
+				.append("text")
+				.attr("x", radius + radius)
+				.attr("y", radius * 1.5)
+				.text((d) => d)
+				.attr("class", "textselected")
+				.style("text-anchor", "start");
+
+			///////////////////////////////////////////////////////////////////////////
+			//////////////////////////// details //////////////////////////////////////
+			///////////////////////////////////////////////////////////////////////////
+
+			function on(f) {
+				document.getElementById("overlay").style.display = "block";
+				var detailsHtml = Mustache.render(template, f);
+				d3.select("#overlay").html(detailsHtml);
+			}
+
+			// function off() {
+			// 	document.getElementById("overlay").style.display = "none";
+			// }
+		};
+		dots(data);
+	};
+
+	update();
+
+	d3.select(window).on("resize", (event) => {
+		update();
+	});
 };
 
 createChart();
